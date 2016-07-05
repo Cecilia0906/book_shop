823476dedc0401e5f5c2c302ad179a62b8b1799d
Cecilia0906 <cecilia0906@gmail.com>
2016-07-05 17:46:07 -0300

    cambios footer

--- resources/views/layoutfrontend.blade.php
+++ resources/views/layoutfrontend.blade.php
@@ -40,9 +40,9 @@
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
-        @include('partials.footer')
+       
      </div>
-
+     @include('partials.footer')
         @yield('script')
 </body>
 </html>
